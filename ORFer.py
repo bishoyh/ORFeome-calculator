230e5f1af6b99839cfe53e715160ed10759ee2e6
Bishoy Hanna <bishoyh@hotmail.com>
2016-04-08 23:39:54 -0400

    adding args and bug fixes

--- ORFer.py
+++ ORFer.py
@@ -1,6 +1,10 @@
 """Find ORFs in Genomes and calculate their length."""
 from Bio import SeqIO
 import matplotlib.pyplot as plt
+import sys
+import getopt as gtopt
+
+
 
 plt.figure(figsize=(12, 9))
 ax = plt.subplot(111)
@@ -11,20 +15,22 @@ ax.get_yaxis().tick_left()
 plt.xlabel("Orf Size", fontsize=16)
 plt.ylabel("Count", fontsize=16)
 
-handle = open("ciona.allmasked", "rU")  # Change to point to the correct genome file
 
 table = 11
-min_pro_len = 100
+min_pro_len = 60
 
 
 # Modified From biopython cookbook recipe 19.1.13
 
 
 def find_orfs_with_trans(seq, trans_table, min_protein_length):
-    """Find orfs and translate them."""
+    """Find orfs and translate them.
+    :type seq: object
+    """
 
     answer = []
     seq_len = len(seq)
+
     for strand, nuc in [(+1, seq), (-1, seq.reverse_complement())]:
         for frame in range(3):
             trans = str(nuc[frame:].translate(trans_table))
@@ -46,24 +52,55 @@ def find_orfs_with_trans(seq, trans_table, min_protein_length):
                                    trans[aa_start:aa_end]))
                 aa_start = aa_end + 1
     answer.sort()
+    #print answer
     return answer
 
 
-L = []
-chars = set('X')
-for record in SeqIO.parse(handle, "fasta"):
-    print record.id
-    orf_list = find_orfs_with_trans(record.seq, table, min_pro_len)
-    for start, end, strand, pro in orf_list:
-        if 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' in pro:  # ignore proteins that spanned masked regions
-            print 'Skipped one \n'
-        continue
-        L.append(len(pro) * 3)
-        print("%s...%s - length %i, strand %i, %i:%i,%s" % (pro[:100], pro[-3:], len(pro) * 3, strand, start, end, record.id))
-handle.close()
-f = open('outputcionamaskedXXXX.txt', 'w')
-for ele in L:
-    f.write(str(ele) + '\n')
-f.close
-plt.hist(L, color="#3F5D7D", bins=100)
-plt.savefig("cionamasked.png", bbox_inches="tight")
+def parse(fastafile, output_filename):
+    handle = open(fastafile, "rU")  # Change to point to the correct genome file
+
+    L = []
+    chars = set('X')
+    for record in SeqIO.parse(handle, "fasta"):
+        print record.id
+        orf_list = find_orfs_with_trans(record.seq, table, min_pro_len)
+        for start, end, strand, pro in orf_list:
+            if 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' in pro:  # ignore proteins that spanned masked regions
+               # print("%s...%s - length %i, strand %i, %i:%i,%s" % (pro[:100], pro[-3:], len(pro) * 3, strand, start, end, record.id))
+                print 'Skipped one \n'
+                continue
+            L.append(len(pro) * 3)
+            #print("%s...%s - length %i, strand %i, %i:%i,%s" % (pro[:100], pro[-3:], len(pro) * 3, strand, start, end, record.id))
+    handle.close()
+
+    f = open(output_filename, 'w')
+    for ele in L:
+        f.write(str(ele) + '\n')
+    f.close
+    plt.hist(L, color="#3F5D7D", bins=100)
+    png_filename = output_filename + ".png"
+    plt.savefig(png_filename, bbox_inches="tight")
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = gtopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except gtopt.GetoptError:
+        print 'ORFer.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print 'Input file is "', inputfile
+    print 'Output file is "', outputfile
+    parse(inputfile, outputfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
